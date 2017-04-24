56e73678cb12d0abbbf5886d877e9ebf312de0c2
Prashant Palikhe <prashpal09@gmail.com>
2017-04-24 15:58:13 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# elementIsReady
-Do something when an element is ready in the DOM
+# element-is-ready
+
+Do something when an element of a given selector is created and added to the DOM.
+
+## Install
+
+$ npm install --save element-is-ready
+
+## Usage
+
+```
+const elementIsReady = require('element-is-ready');
+```
+
+
---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Returns a promise that is resolved when an element with given
+ * selector is created and added to the DOM.
+ *
+ * @param selector Selector of the element to check the ready state of.
+ * @returns {Promise}
+ */
+function elementIsReady(selector) {
+    return new Promise((resolve) => {
+         const element = document.querySelector(selector);
+
+         if (element !== null) {
+             return resolve(element);
+         }
+
+         if ('MutationObserver' in window) {
+            const observer = new MutationObserver(mutation => {
+                const nodes = [...mutation.addedNodes];
+
+                for (let node of nodes) {
+                    if (node.matches && node.matches(selector)) {
+                        observer.disconnect();
+
+                        return resolve(node);
+                    }
+                }
+            });
+
+            observer.observe(document.documentElement, {
+                childList: true,
+                subtree: true
+            });
+
+         } else {
+            const waitInterval = setInterval(() => {
+                const element = document.querySelector(selector);
+
+                if (element) {
+                    clearInterval(waitInterval);
+                    
+                    return resolve(element);
+                }
+            }, 50);
+         }
+    });
+}
+
+export default elementIsReady;
